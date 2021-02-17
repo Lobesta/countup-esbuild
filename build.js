5ca64971b207d6140c97acfce06e0344dd79da0f
Lobesta <kane.taka35@gmail.com>
2021-02-17 19:48:55 +0900

    ビルドオプション修正

--- build.js
+++ build.js
@@ -1,6 +1,7 @@
 const { argv } = require("process")
 const { build } = require("esbuild")
 const path = require("path")
+const fs = require("fs")
 
 const IS_DEV = argv[2] === "development"
 
@@ -8,11 +9,12 @@ const IS_DEV = argv[2] === "development"
 const serverOptions = {
   entryPoints: [path.resolve(__dirname, "src/index.ts")],
   minify: !IS_DEV,
-  bundle: false,
+  bundle: true,
   target: "es2020",
   platform: "node",
   outdir: path.resolve(__dirname, "dist"),
-  tsconfig: path.resolve(__dirname, "tsconfig.json")
+  tsconfig: path.resolve(__dirname, "tsconfig.json"),
+  external: fs.readdirSync("./node_modules")
 }
 build(serverOptions).catch(err => {
   process.stderr.write(err.stderr)
